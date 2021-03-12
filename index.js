cb48bc32ae9664703fa4941837d185d700e42ac5
Home <home@Rosss-MacBook-Pro-2.local>
2021-03-12 17:05:31 +0000

    Remove proccess.env.port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = process.env.port || 3000;
+const port = 3000;
 
 app.use(express.static(__dirname + '/public/'));
 
